ae96650ecca045edaf1592ccd46b957e80f5df72
Philippe Miossec <pmiossec@gmail.com>
2023-05-19 16:45:55 +0200

    feat: Define squarred board game class and handle click and type of cells

--- popit.js
+++ popit.js
@@ -1,5 +1,32 @@
 console.log("popit!!!!!")
 
-function popBubble() {
-    console.log("pop");
+const BubbleState = {
+    popped: 0,
+    selected: 1,
+    untouched: 2
 }
+
+class Board {
+    constructor(board) {
+        this.board = board;
+    }
+}
+
+class SquareBoard extends Board {
+    constructor(size) {
+        super(new Array(size).fill(BubbleState.untouched).map(() => new Array(size).fill(BubbleState.untouched)));
+    }
+
+    popBubble(iCell) {
+        const row = Math.floor(iCell / 6);
+        const column = iCell % 6;
+        if (this.board[row][column] === BubbleState.untouched) {
+            this.board[row][column] = BubbleState.selected;
+        }
+        console.log("pop", iCell, row, column, this.board);
+    }
+}
+
+
+
+const popGame = new SquareBoard(6);
