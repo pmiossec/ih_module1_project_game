e61bccf07539838c4a83d85ccb467f76555f4b76
Philippe Miossec <pmiossec@gmail.com>
2023-05-22 18:11:49 +0200

    Refactor: store cells as array of array
    to prepare single player mode

--- js/popit.js
+++ js/popit.js
@@ -8,10 +8,11 @@ const BubbleState = {
 }
 
 class Board {
-    constructor(cellsCount, players) {
-        this.board = this.createCells(cellsCount);
+    constructor(board, players) {
+        this.board = board;
+        this.uiElements = this.createCells(board);
 
-        this.selectedCells = [];
+        // this.selectedCells = [];
 
         this.players = players;
         if(players.length === 1) {
@@ -24,22 +25,24 @@ class Board {
         this.messageElement =  document.getElementById("user-message");
     }
 
-    createCells(cellsCount) {
+    createCells(board) {
         const cells = []
         const boardDiv = document.getElementById("board");
 
-        while (boardDiv.firstChild) {
-            boardDiv.removeChild(boardDiv.firstChild);
-        }
+        boardDiv.innerHTML = "";
 
-        for (let i = 0; i < cellsCount; i++) {
-            cells[i] = BubbleState.untouched;
-            let div = document.createElement("div");
-            div.setAttribute("class", "untouched");
-            div.setAttribute("id", `c${i}`);
-            div.addEventListener("click", () => this.popBubble(i));
-            boardDiv.appendChild(div);
-            // <div id="c0" class="untouched" onclick="javascript:popGame.popBubble(0)"></div>
+        for (let iRow = 0; iRow < board.length; iRow++) {
+            cells[iRow] = [];
+            for (let iCol = 0; iCol < board[iRow].length; iCol++) {
+                let div = document.createElement("div");
+                div.setAttribute("class", board[iRow][iCol]);
+                div.setAttribute("id", `c${iRow}_${iCol}`);
+                if(board[iRow][iCol] !== BubbleState.unused) {
+                    div.addEventListener("click", () => this.popBubble(iRow, iCol));
+                }
+                boardDiv.appendChild(div);
+                cells[iRow][iCol] = div;
+            }
         }
 
         return cells;
@@ -66,18 +69,29 @@ class Board {
     clearUserMessage() {
         this.messageElement.innerHTML = "&nbsp;";
     }
-    
+
+    countCellsWithState(state) {
+        return this.board.reduce(((acc, l) => acc + l.filter(e => e === state).length), 0);
+    }
+
+    countSelected() {
+        return this.countCellsWithState(BubbleState.selected);
+    }
+
     switchPlayer() {
-        if (this.selectedCells.length === 0) {
+        if (this.countSelected() === 0) {
             this.displayUserMessage("You have to select at least a cell");
             return;
         }
 
         // console.log("start processing cells...");
         
-        for (let i = 0; i < this.selectedCells.length; i++) {
-            let iCell = this.selectedCells[i];
-            this.changeCellState(iCell, BubbleState.popped);
+        for (let iRow = 0; iRow < this.board.length; iRow++) {
+            for (let iCol = 0; iCol < this.board[iRow].length; iCol++) {
+                if(this.board[iRow][iCol] === BubbleState.selected) {
+                    this.changeCellState(iRow, iCol, BubbleState.popped);
+                }
+            }
         }
 
         // console.log("cell processed!!!");
@@ -94,29 +108,19 @@ class Board {
     }
 
     resetBoard() {
-        for (let iCell = 0; iCell < this.board.length; iCell++) {
-            const cell = this.board[iCell];
-            if (cell !== BubbleState.unused) {
-                this.board[iCell] = BubbleState.untouched;
-                this.changeCellState(iCell, BubbleState.untouched);
+        for (let iRow = 0; iRow < this.board.length; iRow++) {
+            for (let iCol = 0; iCol < this.board[iRow].length; iCol++) {
+                if (this.board[iRow][iCol] !== BubbleState.unused) {
+                    this.changeCellState(iRow, iCol, BubbleState.untouched);
+                }
             }
         }
 
-        this.selectedCells = [];
-    }
-
-    canAddOneMore(iCell) {
-        // Max 3 cells!
-        if (this.selectedCells.length === 3 && !this.selectedCells.includes(iCell)) {
-            this.displayUserMessage("3 cells max!!");
-            return false;
-        }
-
-        return true;
+        // this.selectedCells = [];
     }
 
     isGameFinished() {
-        if(this.board.filter(c => c === BubbleState.untouched).length === 1)
+        if(this.countCellsWithState(BubbleState.untouched) === 1)
         {
             const iWinner = (this.activePlayer + 1) % 2;
             const winner = this.players[iWinner];
@@ -128,110 +132,119 @@ class Board {
         return false;
     }
 
-    popBubble(iCell) {
-        if (this.board[iCell] === BubbleState.unused
-             || this.board[iCell] === BubbleState.popped) {
+    popBubble(iRow, iCol) {
+        if (this.board[iRow][iCol] === BubbleState.unused
+             || this.board[iRow][iCol] === BubbleState.popped) {
             return;
         }
 
         this.clearUserMessage();
 
-        if (this.selectedCells.includes(iCell)) {
-            this.selectedCells.splice(this.selectedCells.indexOf(iCell), 1);
-            this.changeCellState(iCell, BubbleState.untouched);
+        if (this.board[iRow][iCol] === BubbleState.selected) {
+            // this.selectedCells.splice(this.selectedCells.indexOf(iCell), 1);
+            this.changeCellState(iRow, iCol, BubbleState.untouched);
             console.log("this.selectedCells (after remove)", this.selectedCells);
-        return;
-        }
-
-        this.selectedCells.push(iCell);
-        console.log("this.selectedCells (after add)", this.selectedCells);
-        this.changeCellState(iCell, BubbleState.selected);
-        //this.audio.play();
-    }
-
-    changeCellState(iCell, newState) {
-        this.board[iCell] = newState;
-        console.log("cell:", iCell, "new state:", newState, "board:", this.board);
-        document.getElementById(`c${iCell}`).className = newState;
-    }
-
-    undo(event) {
-        const iCell = this.selectedCells.pop();
-        this.changeCellState(iCell, BubbleState.untouched);
-    }
-}
-
-class SquareBoard extends Board {
-    constructor(size, players) {
-        super(size * size, players);
-        this.lineSize = size;
-    }
-
-    getRowOfCell(iCell) {
-        return Math.floor(iCell / this.lineSize);
-    }
-
-    popBubble(iCell, elementId) {
-        const iRow = this.getRowOfCell(iCell);
-        console.log("iRow:", iRow);
-        if(!super.canAddOneMore(iCell)) {
             return;
         }
 
-        if (this.selectedCells.length !== 0 && this.getRowOfCell(this.selectedCells[0]) !== iRow) {
+        const countSelected = this.countSelected();
+        console.log("count selected", countSelected);
+        if (countSelected === 3) {
+            this.displayUserMessage("3 cells max!!");
+            return false;
+        }
+
+        //TODO: check cell is on same line
+        if (countSelected !== 0 && this.board[iRow].filter(e => e === BubbleState.selected).length === 0) {
             this.displayUserMessage("All the cells selected should be on the same line!");
             return;
         }
 
-        super.popBubble(iCell, elementId);
+        // this.selectedCells.push(iCell);
+        console.log("this.selectedCells (after add)", this.selectedCells);
+        this.changeCellState(iRow, iCol, BubbleState.selected);
+        //this.audio.play();
     }
 
-    switchPlayer() {
-        super.switchPlayer();
+    changeCellState(iRow, iCol, newState) {
+        this.board[iRow][iCol] = newState;
+        console.log("cell:", iRow, iCol, "new state:", newState, "board:", this.board);
+        this.uiElements[iRow][iCol].className = newState;
+    }
+
+    // undo(event) {
+    //     const iCell = this.selectedCells.pop();
+    //     this.changeCellState(iCell, BubbleState.untouched);
+    // }
+}
+
+class SquareBoard extends Board {
+    constructor(size, players) {
+        const board = [];
+        for (let iRow = 0; iRow < size; iRow++) {
+            board[iRow] = [];
+            for (let iCol = 0; iCol < size; iCol++) {
+                board[iRow][iCol] = BubbleState.untouched;
+            }
+        }
+
+        super(board, players);
     }
 }
 
-
 class LineBoard extends Board {
     constructor(size, players) {
-        super(size, players);
-        this.lineSize = size;
-    }
-
-    popBubble(iCell, elementId) {
-        if(!super.canAddOneMore(iCell)) {
-            return;
+        const board = [];
+        board[0] = [];
+        for (let iCol = 0; iCol < size; iCol++) {
+            board[0][iCol] = BubbleState.untouched;
         }
 
-        super.popBubble(iCell, elementId);
-    }
-
-    switchPlayer() {
-        super.switchPlayer();
+        super(board, players);
+        // this.lineSize = size;
     }
 }
 
 class DiamondBoard extends SquareBoard {
     constructor(size, players) {
-        super(size, players);
 
+        const board = [];
         const limit = (size - 1) / 2;
         for(let i = 0; i < limit; i++) {
             super.changeCellState(0, BubbleState.unused);
         }
-        
+        super(size, players);
     }
 
-    getRowOfCell(iCell) {
-        return Math.floor(iCell / this.lineSize);
-    }
+    // getRowOfCell(iCell) {
+    //     return Math.floor(iCell / this.lineSize);
+    // }
 
-    popBubble(iCell, elementId) {
-        super.popBubble(iCell, elementId);
-    }
+    // popBubble(iCell, elementId) {
+    //     super.popBubble(iCell, elementId);
+    // }
 
-    switchPlayer() {
-        super.switchPlayer();
+    // switchPlayer() {
+    //     super.switchPlayer();
+    // }
+}
+
+class FreeBoard extends Board {
+    constructor(map, players) {
+        console.log("map", map);
+        const rows = map.split("\n");
+        const board = [];
+        for(let iRow = 0; iRow < rows.length; iRow++) {
+            board[iRow] = [];
+            for(let iCol = 0; iCol < rows[iRow].length; iCol++) {
+                board[iRow][iCol] = rows[iRow][iCol] === 'x' ? BubbleState.untouched : BubbleState.unused;
+            }
+        }
+
+        console.log("freeboard", board);
+        super(board, players);
+        this.rowsCount = rows.length;
+        this.colsCount = rows[0].length;
     }
 }
 
@@ -249,6 +262,7 @@ boardChoice.innerHTML = `<option value="line-5">Line 5</option>
 <option value="square-5" selected>Square 5x5</option>
 <option value="square-6">Square 6x6</option>
 <option value="square-7">Square 7x7</option>
+<option value="custom1">Custom1</option>
 `;
 /* <option value="diamond-3">Diamond 3</option>
 <option value="diamond-5">Diamond 5</option>
@@ -283,6 +297,13 @@ function boardSelectionChanged() {
         gridRows = size;
         popGame = new DiamondBoard(size, getPlayers());
 
+    } else if(selectedValue === "custom1"){
+        // console.log("Should not happen!!!");
+        popGame = new FreeBoard(`_x_
+x_x
+_x_`, getPlayers());
+        gridRows = popGame.rowsCount;
+        gridColumns = popGame.colsCount;
     } else {
         console.log("Should not happen!!!");
     }
