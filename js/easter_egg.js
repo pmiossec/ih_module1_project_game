ddd42e2069bc9d38b68b0e9c2b3b2b9c87dd0d9c
Philippe Miossec <pmiossec@gmail.com>
2023-05-24 08:52:54 +0200

    feat: finish board setup and create player

--- js/easter_egg.js
+++ js/easter_egg.js
@@ -2,8 +2,9 @@ console.log("hurry!")
 
 const cells = document.getElementsByClassName("direction");
 
-const directions = ["left", "up", "right", "bottom"];
-
+const directions = ["left", "up", "right", "down"];
+const position = {x: 1, y: 1};
+const offset = (51 - 33)/2;
 [...cells].forEach(c => {
     c.classList.add(directions[Math.floor(Math.random() * 4)]);
     c.addEventListener('click', () => {
@@ -13,3 +14,9 @@ const directions = ["left", "up", "right", "bottom"];
         c.classList.replace(currentValue, newValue);
     })
 });
+
+const startCell = document.getElementById("start");
+startCell.classList.replace("start", "right");
+
+const player = document.getElementById("player");
+let playerDirection = "right";
